f3e358240920f75dfe1253bdf8a68aa55ea8e17a
Sudarikov_Iaroslav <yarik846@mail.ru>
2022-11-07 15:23:21 +0300

    feat: добавлен commentPatchDto

--- ewm-service/src/main/java/ru/practicum/ms/dto/comment/CommentPatchDto.java
+++ ewm-service/src/main/java/ru/practicum/ms/dto/comment/CommentPatchDto.java
@@ -1,4 +1,27 @@
 package ru.practicum.ms.dto.comment;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import ru.practicum.ms.util.PatchValidMarker;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentPatchDto {
+
+    private static final int MIN_TEXT_LEN = 10;
+    private static final int MAX_TEXT_LEN = 1024;
+
+    @NotNull(groups = PatchValidMarker.class)
+    private Long id;
+    @Length(min = MIN_TEXT_LEN, max = MAX_TEXT_LEN, groups = PatchValidMarker.class)
+    private String text;
+    private Long ownerId;
+    private Long eventId;
 }
